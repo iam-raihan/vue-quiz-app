49ebc0dd1428df3150ff9ec2c3cc3fecced7ecec
Raihan Al-Mamun <mdraihan.dev@gmail.com>
2019-06-30 10:58:20 -0700

    Vuex configured

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import QuizModule from "./QuizModule";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  modules: {
+    QuizModule
+  },
   state: {},
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {}
 });
