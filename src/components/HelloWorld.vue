a23c0c52d5d89521952dd86b359d01483b549eb9
Raihan Al-Mamun <mdraihan.dev@gmail.com>
2019-06-30 08:24:17 -0700

    toastr added and configured as dependency

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button class="btn btn-danger">Test Button</button>
+    <button @click="clicked" class="btn btn-danger">Test Button</button>
     <br />
     <b-button variant="outline-primary">Another Test Button</b-button>
   </div>
@@ -12,24 +12,17 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  methods: {
+    clicked() {
+      console.log("clicked");
+      this.$toast.error({
+        message: "Wrong Answer!"
+      });
+      this.$toast.success({
+        message: "Correct Answer!"
+      });
+    }
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,19 @@ import BootstrapVue from "bootstrap-vue";
 
 Vue.use(BootstrapVue);
 
+// adding toastr
+import "cxlt-vue2-toastr/dist/css/cxlt-vue2-toastr.css";
+import CxltToastr from "cxlt-vue2-toastr";
+
+Vue.use(CxltToastr, {
+  position: "top right",
+  timeOut: 3000,
+  progressBar: true,
+  showMethod: "slideInRight",
+  hideMethod: "slideOutRight",
+  title: "Notification"
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
